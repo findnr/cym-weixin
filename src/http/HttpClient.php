1693b8fbd17c1175c3b8f3322dbc6342b31f04f3
cym504875043 <504875043@qq.com>
2021-04-12 23:36:31 +0800

    add weixin get token

--- src/http/HttpClient.php
+++ src/http/HttpClient.php
@@ -21,8 +21,8 @@
         return $wx_id_arr;
     }
     /**
-	 * 获取登录信息
-	 * @param $arr=[] $arr['appid'];$arr['secret'];$arr['code'];
+	 * 获取token信息
+	 * @param $arr=[] $arr['appid'];$arr['secret'];
      * 
 	 * @return array 获取到的token数据
 	 */
@@ -33,5 +33,35 @@
         $wx_token_arr=json_decode($wx_token_string,true);
         return $wx_token_arr;
     }
-    
+    /**
+	 * 获取身份证信息信息
+	 * @param $arr=[] $arr['appid'];$arr['secret'];$arr['code'];
+     * 
+	 * @return array 获取到的token数据
+	 */
+    public function getIdCar($data=[]){
+        $url="https://api.weixin.qq.com/cv/ocr/idcard";
+        return $this->_sed_post($url,$data);
+    }
+    /**
+	 * 发送POST请示
+	 * @param $url string $arr['appid'];$arr['secret'];$arr['code']
+     * @param $data=[] array
+     * 
+	 * @return array 获取到的token数据
+	 */
+    private function _sed_post($url='',$data=[]){
+        $postData=http_build_query($data);
+        $options=[
+            'https'=>[
+                'method'=>'POST',
+                'header' => 'Content-type:application/x-www-form-urlencoded',
+                'content' => $postData,
+                'timeout' => 15 * 60
+            ]
+        ];
+        $context = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        return $result;
+    }
  }
